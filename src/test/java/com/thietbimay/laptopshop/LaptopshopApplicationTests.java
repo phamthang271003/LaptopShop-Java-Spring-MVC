f54a7916f835b78692dbafa6d8dede2637fcbb82
phamthang271003 <phamtranquangthang30@gmail.com>
2024-07-08 11:09:39 +0700

    init project

---
+++ src/test/java/com/thietbimay/laptopshop/LaptopshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thietbimay.laptopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
